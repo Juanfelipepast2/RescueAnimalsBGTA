1da07b30c4ad3619d528b7417916dcdbb78dee94
Felipe <jfpastrana.uninorte.edu.co>
2022-02-02 12:16:45 -0500

    Agregacion libreria sqlite al pom.xml y al mudule-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module carpetas {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires java.sql;
 
     opens carpetas.GUI to javafx.fxml;
     exports carpetas.GUI;
