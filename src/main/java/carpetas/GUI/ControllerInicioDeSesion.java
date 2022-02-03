b6922bce3f9de476d2e7f17a5c7cdd402789aeb2
fchaparroa <fhchaparroa@gmail.com>
2022-02-02 22:17:10 -0500

    Aguegue el codigo del Controlador InicioDeSesion

---
+++ src/main/java/carpetas/GUI/ControllerInicioDeSesion.java
@@ -0,0 +1,34 @@
+package carpetas.GUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ControllerInicioDeSesion {
+
+    @FXML
+    private Button BtnIniciarSesion;
+
+    @FXML
+    private Button BtnRegistro;
+
+    @FXML
+    private CheckBox CheckBoxClave;
+
+    @FXML
+    private TextField TextClave;
+
+    @FXML
+    private PasswordField TextClave2;
+
+    @FXML
+    private TextField TextUsuario;
+
+    @FXML
+    void IniciarSesionClic(ActionEvent event) {
+
+    }
+
+}
