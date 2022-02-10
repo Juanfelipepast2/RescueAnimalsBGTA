4881d7247e677c975573d5819c89e7ea334517e3
Davidrg02 <davrodriguezgu@unal.edu.co>
2022-02-09 23:28:36 -0500

    Creación vista lista de animales fundación

---
+++ src/main/java/carpetas/GUI/ControladorItemAnimalFundacion.java
@@ -0,0 +1,21 @@
+package carpetas.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ControladorItemAnimalFundacion {
+
+    @FXML
+    private ImageView fotoAnimalItemFundacion;
+
+    @FXML
+    private Label nombreAnimalItemFundacion;
+
+    @FXML
+    private Label razaAnimalItemFundacion;
+
+    @FXML
+    private Label tipoAnimalItemFundacion;
+
+}
---
+++ src/main/java/carpetas/GUI/ControladorListaAnimalesFundacion.java
@@ -0,0 +1,11 @@
+package carpetas.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class ControladorListaAnimalesFundacion {
+
+    @FXML
+    private GridPane gridAnimalesFundacion;
+
+}
